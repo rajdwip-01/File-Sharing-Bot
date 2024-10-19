e3f83bc09bc88b87f3e1a4cdeeb4d186a875e7e4
rajdwip-01 <saharajdwip906@gmail.com>
2024-10-19 22:57:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7892314823:AAFgTq8xmrlzO-K6fd3f1olvhaJNExjqyrA")
-
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "25626616"))
 
@@ -32,7 +31,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://saharajdwip906:<db_passwo
 DB_NAME = os.environ.get("DATABASE_NAME", "Uzumaki")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002300137332"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
